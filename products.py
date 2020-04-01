8577d563ac5fa8ea3925372f98f57c3e85bb3f8b
Eric Lin <lemon198926@gmail.com>
2020-04-01 22:00:49 +0800

    second commit

--- products.py
+++ products.py
@@ -1,4 +1,5 @@
 products = []
+money = 0
 
 while True:
     name = input("請輸入商品名稱： ")
@@ -6,6 +7,13 @@ while True:
         break
     price = input("請輸入價格： ")
     products.append([name, price])
+    money += int(price)
 
 for p in products:
     print(p[0], "的價格是：", p[1], "元")
+
+print("共花", money, "元")
+
+with open ("products.csv" , "w" , encoding = "utf-8") as f:
+    for d in products:
+        f.write(d[0] + "," + str(d[1]) + "元" + "\n")
