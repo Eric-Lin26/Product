ab2deefbf94d43c477dab302b3bc4ed58b3b4967
Eric-Lin26 <61929286+Eric-Lin26@users.noreply.github.com>
2020-04-04 21:51:20 +0800

    更新第三版
    增加檢查檔案功能，及讀取檔案功能

--- products.py
+++ products.py
@@ -1,5 +1,19 @@
+import os #operating system
 products = []
-money = 0
+if os.path.isfile("products.csv"): # os模組path功能中的isfile去尋找檔案
+    print("找到檔案了！")
+    # 讀取檔案
+    with open("products.csv", "r", encoding = "utf-8") as f:
+        for line in f:
+            if "商品,價格" in line:
+                continue
+            name, price = line.strip().split(",") #.strip() 去除\n換行符號 #.split()切割
+            products.append([name, price])
+    print(products)
+else:
+    print("沒找到檔案！")
+
+# 使用者輸入
 
 while True:
     name = input("請輸入商品名稱： ")
@@ -7,13 +21,13 @@ while True:
         break
     price = input("請輸入價格： ")
     products.append([name, price])
-    money += int(price)
 
 for p in products:
     print(p[0], "的價格是：", p[1], "元")
 
-print("共花", money, "元")
 
+# 寫入檔案
 with open ("products.csv" , "w" , encoding = "utf-8") as f:
+    f.write("商品,價格\n")
     for d in products:
-        f.write(d[0] + "," + str(d[1]) + "元" + "\n")
+        f.write(d[0] + "," + str(d[1]) + "\n")
